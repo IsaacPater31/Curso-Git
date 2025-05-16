548e7eef2ceb2c79c57bb19f14e3922ec0e71fb3
Luigi Salcedo <cesarluigisalcedo@gmail.com>
2025-05-16 14:01:56 -0500

    test agregados

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 console.log("Hello, World!");
 console.log("This is a test.");
 console.log("This is a test.");
+conselog.log("This is a test.");
+console.log("This is a test.");
+console.log("This is a test.");
